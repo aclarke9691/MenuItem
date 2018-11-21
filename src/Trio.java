b0295b6d0a20e0715f877a3d2a59ba9507989e9f
aclarke9691 <aclarke9691@bths.edu>
2018-11-21 14:34:11 -0500

    done

--- src/Trio.java
+++ src/Trio.java
@@ -4,4 +4,39 @@ public class Trio implements MenuItem {
     private Drink drink;
     
     public Trio(Sandwich sandwich, Salad salad, Drink drink)
+    {
+        this.sandwich=sandwich;
+        this.salad=salad;
+        this.drink=drink;
+    }
+
+    public String getName()
+    {
+        String sandwichName= this.sandwich.getName();
+        String saladName= this.salad.getName();
+        String drinkName= this.drink.getName();
+        return sandwichName + "/" + saladName + "/" + drinkName + " Trio";
+    }
+
+    public double getPrice()
+    {
+        double sandwichPrice= this.sandwich.getPrice();
+        double saladPrice= this.salad.getPrice();
+        double drinkPrice= this.drink.getPrice();
+        if (sandwichPrice < saladPrice && sandwichPrice< drinkPrice)
+        {
+            return saladPrice+drinkPrice;
+        }
+        else
+        {
+            if (saladPrice<sandwichPrice && saladPrice<drinkPrice)
+            {
+                return sandwichPrice+drinkPrice;
+            }
+            else
+            {
+                return sandwichPrice+saladPrice;
+            }
+        }
+    }
 }
